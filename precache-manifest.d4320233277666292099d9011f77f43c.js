667c8c110f6b067aa23f705dc25fd1df22b67778
Palak Manocha <palakmanocha99@gmail.com>
2019-02-15 17:28:15 +0530

    Updates

---
+++ precache-manifest.d4320233277666292099d9011f77f43c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6473c9243ea43aad7509",
+    "url": "/react-calculator/static/js/runtime~main.6473c924.js"
+  },
+  {
+    "revision": "3302abf236a221e94530",
+    "url": "/react-calculator/static/js/main.3302abf2.chunk.js"
+  },
+  {
+    "revision": "72ae5f8c9ec432a8a9fe",
+    "url": "/react-calculator/static/js/2.72ae5f8c.chunk.js"
+  },
+  {
+    "revision": "3302abf236a221e94530",
+    "url": "/react-calculator/static/css/main.65d982a3.chunk.css"
+  },
+  {
+    "revision": "6b1c5ec8ad59f55cf5f3e8858dc6ab46",
+    "url": "/react-calculator/index.html"
+  }
+];
